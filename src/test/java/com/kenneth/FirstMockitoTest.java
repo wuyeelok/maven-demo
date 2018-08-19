91ac77e9a64be3fb8949c5d79423073b37cfc3f8
wuyeelok <wuyeelok@gmail.com>
2018-08-19 16:29:13 +0800

    Create first Mockito Test

---
+++ src/test/java/com/kenneth/FirstMockitoTest.java
@@ -0,0 +1,19 @@
+package com.kenneth;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FirstMockitoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
